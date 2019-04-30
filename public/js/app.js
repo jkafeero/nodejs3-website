768c890d830644daa54685e36a6840ea3fe5ca2f
Joshua Kafeero <joshua_kafeero@intuit.com>
2019-04-30 07:59:34 -0700

    enhanced forecast with high and low

--- public/js/app.js
+++ public/js/app.js
@@ -24,9 +24,9 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast.summary + ' temperature is '  
                     + data.forecast.temperature +
-                    ' propability of precipitation is ' + data.forecast.precipProbability
-                    + 'with a high of ' +data.forecast.dayHigh + ' and a low of ' + data.forecast.dayLow
-               
+                    'C with propability of precipitation is ' + data.forecast.precipProbability
+                    + '% with a high of ' +data.forecast.dayHigh + 'C and a low of ' + data.forecast.dayLow
+                    + 'C'
                 // messageTwo.textContent = {
                 //     location: data.location,
                 //     forecast: data.forecast
